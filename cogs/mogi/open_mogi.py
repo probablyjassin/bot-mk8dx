35f94643b89d0bd21cbf352e99b538192b05dc5a
probablyjassin <aouanijassin@gmail.com>
2024-10-20 00:28:54 +0200

    create open_mogi.py

---
+++ cogs/mogi/open_mogi.py
@@ -0,0 +1,20 @@
+import random
+import discord
+from discord import slash_command, ApplicationContext
+from discord.ext import commands, tasks
+from utils.mogis import create_mogi
+
+class open_mogi(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @slash_command(name="open", description="Open a mogi")
+    async def open_mogi(self, ctx: ApplicationContext):
+        try:
+            create_mogi(ctx.channel.id)
+            await ctx.send("New Mogi open!")
+        except ValueError:
+            await ctx.send("A Mogi for this channel is already open.")
+
+def setup(bot: commands.Bot):
+    bot.add_cog(open_mogi(bot))
