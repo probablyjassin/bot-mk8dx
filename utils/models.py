dd4ff3b18b5e167e9fa33d7859bc360a239ffbfd
probablyjassin <aouanijassin@gmail.com>
2024-10-20 00:34:47 +0200

    fixed Mogi Model

--- utils/models.py
+++ utils/models.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from utils.database import db
 
 @dataclass
@@ -14,6 +14,12 @@ class PlayerProfile:
 class MogiPlayer:
     discord_id: int
 
+def emtpty_list():
+    return []
+
+def default_team_tags():
+    return ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6"]
+
 @dataclass
 class Mogi:
     channel_id: int
@@ -24,16 +30,16 @@ class Mogi:
 
     player_cap: int = 12
 
-    votes: dict[str, int] = []
-    voters: list[int] = []
+    votes: dict[str, int] = field(default_factory=emtpty_list)
+    voters: list[int] = field(default_factory=emtpty_list)
 
-    players: list[MogiPlayer] = []
-    subs: list[MogiPlayer] = []
-    teams: list[list[MogiPlayer]] = []
-    team_tags: list[str] = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6"] 
+    players: list[MogiPlayer] = field(default_factory=emtpty_list)
+    subs: list[MogiPlayer] = field(default_factory=emtpty_list)
+    teams: list[list[MogiPlayer]] = field(default_factory=emtpty_list)
+    team_tags: list[str] = field(default_factory=default_team_tags)
 
     format: str = ""
 
-    collected_points: list[int] = []
-    calced_results: list[str] = []
-    players_ordered_placements: list[str] = []
+    collected_points: list[int] = field(default_factory=emtpty_list)
+    calced_results: list[str] = field(default_factory=emtpty_list)
+    players_ordered_placements: list[str] = field(default_factory=emtpty_list)
