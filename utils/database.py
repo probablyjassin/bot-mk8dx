8c16883ff309c020a365343e91dadf83495bed9e
probablyjassin <aouanijassin@gmail.com>
2024-10-19 23:21:25 +0200

    fix main.py

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,11 @@
 from pymongo import MongoClient
 from config import MONGO_URI
+import atexit
 
 client = MongoClient(MONGO_URI)
 db = client.get_database("lounge")
+
+def close_client():
+    client.close()
+
+atexit.register(close_client)
