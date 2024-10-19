33bb85093b16d6af738f8d4f78c63899da2bab2b
probablyjassin <aouanijassin@gmail.com>
2024-10-20 01:07:56 +0200

    finish join command

--- cogs/mogi/join_mogi.py
+++ cogs/mogi/join_mogi.py
@@ -1,13 +1,25 @@
-import random
-
-import discord
-from discord.ext import commands, tasks
+import asyncio
+from discord import slash_command, ApplicationContext
+from discord.ext import commands
+from utils.mogis import get_mogi
+from utils.models import Mogi, MogiPlayer
 
 class join_mogi(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
+        self.join_semaphore = asyncio.Semaphore(1)
 
-    
+    @slash_command(name="join", description="Join this mogi")
+    async def join_mogi(self, ctx: ApplicationContext):
+        async with self.join_semaphore:
+            mogi: Mogi = get_mogi(ctx.channel.id)
+            if not mogi:
+                await ctx.respond("There is no mogi open in this channel.")
+            if ctx.author.id in mogi.players:
+                await ctx.respond("You're already in this mogi.")
+            else:
+                mogi.players.append(MogiPlayer(discord_id=ctx.author.id))
+                await ctx.respond(f"{ctx.author.mention} has joined the mogi!\n{len(mogi.players)} players are in!")
 
 def setup(bot: commands.Bot):
     bot.add_cog(join_mogi(bot))
