7d3069cf522189d3886dee2c330f3ff1568bb419
probablyjassin <aouanijassin@gmail.com>
2024-10-20 00:44:08 +0200

    add function to get mogi

--- utils/mogis.py
+++ utils/mogis.py
@@ -7,6 +7,11 @@ def create_mogi(channel_id: int):
         raise ValueError("Mogi with this ID already exists.")
     mogi_registry[channel_id] = Mogi(channel_id=channel_id)
 
+def get_mogi(channel_id: int):
+    if channel_id not in mogi_registry:
+        raise ValueError("Mogi with this ID does not exist.")
+    return mogi_registry[channel_id]
+
 def edit_mogi(channel_id: int, mogi_data: dict):
     if channel_id not in mogi_registry:
         raise ValueError("Mogi with this ID does not exist.")
