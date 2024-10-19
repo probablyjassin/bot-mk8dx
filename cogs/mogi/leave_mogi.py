6c3f8c754747d198b46018b7fc787f9e65ec15d3
probablyjassin <aouanijassin@gmail.com>
2024-10-20 01:23:40 +0200

    add leave_mogi cog

---
+++ cogs/mogi/leave_mogi.py
@@ -0,0 +1,28 @@
+import asyncio
+from discord import slash_command, ApplicationContext
+from discord.ext import commands, tasks
+from utils.mogis import get_mogi, close_mogi
+from utils.models import Mogi, MogiPlayer
+
+class leave_mogi(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+        self.leave_semaphore = asyncio.Semaphore(1)
+
+    @slash_command(name="leave", description="Leave this mogi")
+    async def join_mogi(self, ctx: ApplicationContext):
+        async with self.leave_semaphore:
+            mogi: Mogi = get_mogi(ctx.channel.id)
+            if not mogi:
+                await ctx.respond("There is no mogi open in this channel.")
+            if not [player for player in mogi.players if player.discord_id == ctx.author.id]:
+                await ctx.respond("You're not in this mogi.")
+            else:
+                mogi.players = [player for player in mogi.players if player.discord_id != ctx.author.id]
+                if len(mogi.players) == 0:
+                    close_mogi(ctx.channel.id)
+                    return await ctx.respond("This mogi has been closed.")
+                await ctx.respond(f"{ctx.author.mention} has left the mogi!\n{len(mogi.players)} players are in!")
+
+def setup(bot: commands.Bot):
+    bot.add_cog(leave_mogi(bot))
