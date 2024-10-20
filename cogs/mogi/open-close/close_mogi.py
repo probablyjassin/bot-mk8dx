efdced325259d9cc715723214bff1affd061c3ad
probablyjassin <aouanijassin@gmail.com>
2024-10-20 20:46:44 +0200

    fix import in close_mogi

--- cogs/mogi/open-close/close_mogi.py
+++ cogs/mogi/open-close/close_mogi.py
@@ -2,7 +2,7 @@ from discord import slash_command, ApplicationContext
 from discord.ext import commands
 from utils.mogis import get_mogi
 from utils.mogis import destroy_mogi
-from utils.commandUtils.confirm import confirmation
+from utils.commands.confirm import confirmation
 
 class close_mogi(commands.Cog):
     def __init__(self, bot):
