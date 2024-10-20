f3bf54a7a27e8c9c0c85c08b7abb60aafb1c3ce6
probablyjassin <aouanijassin@gmail.com>
2024-10-20 10:06:54 +0200

    add collections to db, specify db name in env

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv('MONGO_URI')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+MONGO_URI = os.getenv('MONGO_URI')
+LOUNGE_DB = os.getenv('LOUNGE_DB')
--- utils/database.py
+++ utils/database.py
@@ -1,9 +1,11 @@
-from pymongo import MongoClient
-from config import MONGO_URI
 import atexit
+from pymongo import MongoClient
+from config import MONGO_URI, LOUNGE_DB
 
 client = MongoClient(MONGO_URI)
-db = client.get_database("lounge")
+db = client.get_database(LOUNGE_DB)
+players_collection = db.get_collection("players")
+archived_collection = db.get_collection("archived")
 
 def close_client():
     client.close()
