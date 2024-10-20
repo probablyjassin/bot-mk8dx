13dfa59f3ba590a140a28dbd088453be85286252
probablyjassin <aouanijassin@gmail.com>
2024-10-20 18:07:42 +0200

    allow multiple guilds and introduce main guild

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 MONGO_URI = os.getenv('MONGO_URI')
 LOUNGE_DB = os.getenv('LOUNGE_DB')
-GUILD_ID = os.getenv('GUILD_ID')
+GUILD_IDS = [int(guild_id) for guild_id in os.getenv('GUILD_IDS').split(',')]
 ERROR_CHANNEL_ID = os.getenv('ERROR_CHANNEL_ID')
 REGISTER_CHANNEL_ID = os.getenv('REGISTER_CHANNEL_ID')
--- utils/objects.py
+++ utils/objects.py
@@ -1,11 +1,11 @@
 import discord
 from discord import Guild, TextChannel, Role
 from discord.utils import get
-from config import GUILD_ID, ERROR_CHANNEL_ID, REGISTER_CHANNEL_ID
+from config import GUILD_IDS, ERROR_CHANNEL_ID, REGISTER_CHANNEL_ID
 from main import bot
 
-async def get_guild() -> Guild:
-    return await bot.fetch_guild(GUILD_ID)
+async def get_main_guild() -> Guild:
+    return await bot.fetch_guild(GUILD_IDS[0])
 
 async def get_register_channel() -> TextChannel:
     return await bot.fetch_channel(REGISTER_CHANNEL_ID)
@@ -14,16 +14,16 @@ async def get_error_channel() -> TextChannel:
     return await bot.fetch_channel(ERROR_CHANNEL_ID)
 
 async def get_lounge_role(name: str) -> Role:
-    return get((await get_guild()).roles, name=name)
+    return get((await get_main_guild()).roles, name=name)
 
 async def get_inmogi_role() -> Role:
-    return get((await get_guild()).roles, name="InMogi")
+    return get((await get_main_guild()).roles, name="InMogi")
 
 async def get_mogi_manager_role() -> Role:
-    return get((await get_guild()).roles, name="Mogi Manager")
+    return get((await get_main_guild()).roles, name="Mogi Manager")
 
 async def get_moderator_role() -> Role:
-    return get((await get_guild()).roles, name="Moderator")
+    return get((await get_main_guild()).roles, name="Moderator")
 
 async def get_admin_role() -> Role:
-    return get((await get_guild()).roles, name="Admin")
+    return get((await get_main_guild()).roles, name="Admin")
