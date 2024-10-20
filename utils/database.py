45d92c8e958e4667be0fb40ff6520cf8112bbe9a
probablyjassin <aouanijassin@gmail.com>
2024-10-20 10:10:08 +0200

    db logging

--- utils/database.py
+++ utils/database.py
@@ -7,6 +7,8 @@ db = client.get_database(LOUNGE_DB)
 players_collection = db.get_collection("players")
 archived_collection = db.get_collection("archived")
 
+print(f"---\nConnected to MongoDB. Database: {db.name}\n---", )
+
 def close_client():
     client.close()
 
