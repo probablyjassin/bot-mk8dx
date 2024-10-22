a2988236e28510be0a53b8e25f408dbca8a81f33
probablyjassin <aouanijassin@gmail.com>
2024-10-22 17:27:50 +0200

    offload code from team building script

--- utils/maths/teams.py
+++ utils/maths/teams.py
@@ -1,22 +1,22 @@
 from utils.mogis import get_mogi
+from models import PlayerProfile
 
 # INFO: @Jässin, this should be done for the most part but we need to see how the method gets called in the first place.
 # That could change the arguments that need to be passed in for example.
 
-def distribute_players_to_teams(channel_id: int, team_size: int) -> list[list[PlayerProfile]]:
+def distribute_players_to_teams(players_in_mogi: int, team_size: int) -> list[list[PlayerProfile]]:
     """
     Distribute players to teams in a balanced fashion based on their MMR (using greedy approach).
 
-    :param channel_id: ID of the channel to get the player list from.
+    :param players_in_mogi: Array of the players in the mogi.
     :param team_size: Number of players per team.
     :return: List of teams with distributed players.
     """
     # Sort players by MMR in descending order
-    player_list = get_mogi(channel_id).players
-    player_count = len(player_list)
+    player_count = len(players_in_mogi)
     team_count = player_count // team_size
 
-    player_list.sort(key=lambda player: player.mmr, reverse=True)
+    players_in_mogi.sort(key=lambda player: player.mmr, reverse=True)
 
     # Initialize teams
     teams = [[] for _ in range(team_count)]
@@ -25,7 +25,7 @@ def distribute_players_to_teams(channel_id: int, team_size: int) -> list[list[Pl
     team_index = 0
 
     for i in range(player_count):
-        teams[team_index].append(player_list[i])
+        teams[team_index].append(players_in_mogi[i])
 
         if not reverse:
             team_index += 1
