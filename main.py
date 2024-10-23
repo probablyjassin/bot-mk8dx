5602e21af20dda00dcd1f5f4277c61774b353e9f
Jässin <s5002669@ba-sachsen.de>
2024-10-23 10:49:07 +0200

    format main.py

--- main.py
+++ main.py
@@ -13,50 +13,66 @@ intents = discord.Intents.all()
 
 owners = [769525682039947314, 450728788570013721]
 
+
 class customBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         return super().__init__(*args, **kwargs)
 
     async def close(self):
-        for name,cog in self.cogs.items():
+        for name, cog in self.cogs.items():
             cog._eject(self)
             print(f"Ejected {name}")
         await super().close()
 
+
 bot = customBot(
-    command_prefix=".", case_insensitive = True, help_command = None,
-    intents=intents, owner_ids = set(owners), 
-    status=discord.Status.online, activity=discord.Streaming(name="ones and zeroes", url="https://www.youtube.com/watch?v=xvFZjo5PgG0")
+    command_prefix=".",
+    case_insensitive=True,
+    help_command=None,
+    intents=intents,
+    owner_ids=set(owners),
+    status=discord.Status.online,
+    activity=discord.Streaming(
+        name="ones and zeroes", url="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+    ),
 )
+
 
 @bot.event
 async def on_ready():
-    logger.info(f'Logged into Discord')
-    logger.info(f'Latency: {bot.latency*1000:.2f}ms')
-    print(f"""
+    logger.info(f"Logged into Discord")
+    logger.info(f"Latency: {bot.latency*1000:.2f}ms")
+    print(
+        f"""
         {highlight(bot.user)}
         ID: {highlight(bot.user.id)}
-    """)
-    
+    """
+    )
+
     print("Guilds:")
     for guild in bot.guilds:
         print(guild.name)
     print("--------")
 
+
 def load_extensions():
     print("----Loading extensions----")
-    for root, dirs, files in os.walk('./cogs'):
+    for root, dirs, files in os.walk("./cogs"):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswith(".py"):
                 cog_path = os.path.join(root, file)
-                extension = cog_path[2:].replace('/', '.').replace('\\', '.').replace('.py', '')
+                extension = (
+                    cog_path[2:].replace("/", ".").replace("\\", ".").replace(".py", "")
+                )
                 bot.load_extension(extension)
                 print(f"Loaded {extension}")
     print("----Finished loading extensions----")
 
+
 def main():
     load_extensions()
     bot.run(DISCORD_TOKEN)
+
 
 if __name__ == "__main__":
     main()
