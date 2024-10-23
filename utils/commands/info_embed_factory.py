0fa09ca61266133b35426ac64b4f19d9386db439
probablyjassin <aouanijassin@gmail.com>
2024-10-23 13:06:50 +0200

    make info embed inline

--- utils/commands/info_embed_factory.py
+++ utils/commands/info_embed_factory.py
@@ -1,16 +1,10 @@
 from discord import Embed, Color
 
+
 def create_embed(
-    title: str, 
-    description: str, 
-    fields: dict, 
-    color: Color = Color.blue()
+    title: str, description: str, fields: dict, color: Color = Color.blue()
 ) -> Embed:
-    embed = Embed(
-        title=title,
-        description=description,
-        color=color
-    )
+    embed = Embed(title=title, description=description, color=color)
     for name, value in fields.items():
-        embed.add_field(name=name, value=value, inline=False)
+        embed.add_field(name=name, value=value, inline=True)
     return embed
