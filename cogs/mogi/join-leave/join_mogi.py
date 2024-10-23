94c4a4abb8b4dc690592de4aae71239b68f49178
Jässin <s5002669@ba-sachsen.de>
2024-10-23 09:11:35 +0200

    comment join_mogi and check for suspension

--- cogs/mogi/join-leave/join_mogi.py
+++ cogs/mogi/join-leave/join_mogi.py
@@ -18,22 +18,34 @@ class join_mogi(commands.Cog):
     async def join(self, ctx: ApplicationContext):
         async with self.join_semaphore:
             mogi: Mogi = get_mogi(ctx.channel.id)
+            # check if mogi open
             if not mogi:
                 return await ctx.respond("There is no mogi open in this channel.")
+            # check if player already in mogi
             if [player for player in mogi.players if player.discord_id == ctx.author.id]:
                 return await ctx.respond("You're already in this mogi.")
+            # check if mogi full
             if len(mogi.players) >= mogi.player_cap:
                 return await ctx.respond("This mogi is full.")
+            # check if mogi is in progress
             if mogi.isPlaying or mogi.isVoting:
                 return await ctx.respond("This mogi has already started.")
 
+            # fetch player record
             player_entry = db_players.find_one({"discord_id": Int64(ctx.author.id)})
+            # if not found
             if not player_entry:
                 if db_archived.find_one({"discord_id": Int64(ctx.author.id)}):
                     return await ctx.respond("You're in Lounge but archived. Contact a mod to get unarchived.")
                 return await ctx.respond("You're not registered for Lounge.")
             
+            # assign Player object
             player: PlayerProfile = PlayerProfile(**player_entry)
+            
+            # if suspended
+            if player.suspended:
+                return await ctx.respond("You're temporarily inable to join mogis.")
+            
             mogi.players.append(player)
             await ctx.respond(f"{ctx.author.mention} has joined the mogi!\n{len(mogi.players)} players are in!")
 
