0abe2e08f731e4966a1ecaed76e7fce38486ee8a
Jässin <s5002669@ba-sachsen.de>
2024-10-23 11:22:56 +0200

    format and modify MogiModel

--- models/MogiModel.py
+++ models/MogiModel.py
@@ -1,39 +1,54 @@
 from dataclasses import dataclass, field
-from bson.int64 import Int64
-from utils.maths.teams_algorithm import distribute_players_to_teams
+
 from models.PlayerModel import PlayerProfile
 
-def e():
-    return []
+from utils.maths.teams_algorithm import distribute_players_to_teams
 
-def default_votes():
-    return { "ffa": 0, "2v2": 0, "3v3": 0, "4v4": 0, "5v5": 0, "6v6": 0 }
-
-def default_team_tags():
-    return ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6"]
 
 @dataclass
 class Mogi:
+    """
+    ### Represents a Mogi in a channel.
+    """
+
     channel_id: int
+    player_cap: int = 12
+    format: int | None = None
+
+    players: list[PlayerProfile] = field(default_factory=lambda: [])
+    teams: list[list[PlayerProfile]] = field(default_factory=lambda: [])
+    subs: list[PlayerProfile] = field(default_factory=lambda: [])
 
     isVoting: bool = False
     isPlaying: bool = False
     isFinished: bool = False
-    format: int | None = None
 
-    player_cap: int = 12
+    collected_points: list[int] = field(default_factory=lambda: [])
+    calced_results: list[str] = field(default_factory=lambda: [])
+    players_ordered_placements: list[str] = field(default_factory=lambda: [])
 
-    votes: dict[str, int] = field(default_factory=default_votes)
-    voters: list[int] = field(default_factory=e)
+    voters: list[int] = field(default_factory=lambda: [])
+    votes: dict[str, int] = field(
+        default_factory=lambda: {
+            "ffa": 0,
+            "2v2": 0,
+            "3v3": 0,
+            "4v4": 0,
+            "5v5": 0,
+            "6v6": 0,
+        }
+    )
 
-    players: list[PlayerProfile] = field(default_factory=e)
-    subs: list[PlayerProfile] = field(default_factory=e)
-    teams: list[list[PlayerProfile]] = field(default_factory=e)
-    team_tags: list[str] = field(default_factory=default_team_tags)
-
-    collected_points: list[int] = field(default_factory=e)
-    calced_results: list[str] = field(default_factory=e)
-    players_ordered_placements: list[str] = field(default_factory=e)
+    team_tags: list[str] = field(
+        default_factory=lambda: [
+            "Team 1",
+            "Team 2",
+            "Team 3",
+            "Team 4",
+            "Team 5",
+            "Team 6",
+        ]
+    )
 
     def play(self, format_int: int) -> None:
         self.format = format_int
@@ -44,7 +59,7 @@ class Mogi:
 
         else:
             self.teams = distribute_players_to_teams(self.players, format_int)
-        
+
         self.isVoting = False
         self.isPlaying = True
 
