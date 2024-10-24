1b11bc77f7bbe8f584aaf3c49eb0d87092d4787f
Jässin <s5002669@ba-sachsen.de>
2024-10-24 14:31:17 +0200

    fixed logging

--- utils/data/database.py
+++ utils/data/database.py
@@ -1,7 +1,7 @@
 import atexit
 from pymongo import MongoClient
 from config import MONGO_URI, LOUNGE_DB
-from bson.int64 import Int64
+from main import logger
 
 
 client = MongoClient(MONGO_URI)
@@ -9,8 +9,8 @@ db = client.get_database(LOUNGE_DB)
 db_players = db.get_collection("players")
 db_archived = db.get_collection("archived")
 
-print(
-    f"***Connected to MongoDB. Database: {db.name}***",
+logger.info(
+    f"***\nConnected to MongoDB. Database: {db.name}\n***",
 )
 
 
