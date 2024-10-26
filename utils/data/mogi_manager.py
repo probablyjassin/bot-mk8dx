479a4c07fd83b02a8c9ef2ae7c2d1ffdf816db95
probablyjassin <aouanijassin@gmail.com>
2024-10-26 16:34:28 +0200

    format mogi_manager

--- utils/data/mogi_manager.py
+++ utils/data/mogi_manager.py
@@ -2,13 +2,16 @@ from models.MogiModel import Mogi
 
 mogi_registry = {}
 
+
 def create_mogi(channel_id: int) -> None:
     if channel_id in mogi_registry:
         raise ValueError("Mogi with this ID already exists.")
     mogi_registry[channel_id] = Mogi(channel_id=channel_id)
 
+
 def get_mogi(channel_id: int) -> Mogi | None:
-   return mogi_registry.get(channel_id, None)
+    return mogi_registry.get(channel_id, None)
+
 
 def destroy_mogi(channel_id: int) -> None:
     if channel_id not in mogi_registry:
