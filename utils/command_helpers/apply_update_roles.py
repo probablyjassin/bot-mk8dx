bec8a5646e56486c6b3aa73679e16c5b057aa1a8
probablyjassin <aouanijassin@gmail.com>
2024-10-27 10:29:16 +0100

    add code to apply roles and exclude subs

---
+++ utils/command_helpers/apply_update_roles.py
@@ -0,0 +1,38 @@
+from discord import ApplicationContext, Member
+from discord.utils import get
+
+from models.MogiModel import Mogi
+from utils.maths.ranks import getRankByMMR
+
+
+async def update_roles(
+    ctx: ApplicationContext,
+    mogi: Mogi,
+):
+    all_player_names = [player.name for player in mogi.players]
+
+    for player in mogi.players:
+        discord_member: Member = get(ctx.guild.members, id=int(player.discord_id))
+        if not discord_member:
+            await ctx.send(f"Skipped {player.name}, couldn't find member in server.")
+            continue
+
+        if player in mogi.subs:
+            await ctx.send(f"Excluded {discord_member.mention} because they subbed")
+            continue
+
+        current_rank = getRankByMMR(player.mmr)
+        new_rank = getRankByMMR(
+            player.mmr + mogi.mmr_results_by_group[mogi.players.index(player)]
+        )
+
+        if current_rank != new_rank:
+
+            await ctx.send(f"{discord_member.mention} is now in {new_rank.name}")
+
+            await discord_member.remove_roles(
+                get(ctx.guild.roles, name=f"Lounge - {current_rank}")
+            )
+            await discord_member.add_roles(
+                get(ctx.guild.roles, name=f"Lounge - {new_rank}")
+            )
