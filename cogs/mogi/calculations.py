d519661fa827c22a440d6adcf8e5a66e667d16f0
probablyjassin <aouanijassin@gmail.com>
2024-10-31 22:51:16 +0100

    implement MogiAppCtx in calculations

--- cogs/mogi/calculations.py
+++ cogs/mogi/calculations.py
@@ -9,6 +9,7 @@ from discord import (
 )
 from discord.ext import commands
 
+from models.CustomMogiContext import MogiApplicationContext
 from models.MogiModel import Mogi
 from utils.data.mogi_manager import mogi_manager
 
@@ -39,10 +40,8 @@ class calculations(commands.Cog):
     @is_mogi_open()
     @is_mogi_in_progress()
     @is_mogi_manager()
-    async def collect(self, ctx: ApplicationContext):
+    async def collect(self, ctx: MogiApplicationContext):
         await ctx.response.defer()
-
-        mogi: Mogi = mogi_manager.get_mogi(ctx.channel.id)
 
         # Create a thread to collect points
         points_collection_thread: Thread = await ctx.channel.create_thread(
@@ -65,21 +64,23 @@ class calculations(commands.Cog):
 
         # Collect the points to the mogi
         try:
-            mogi.collect_points(tablestring)
+            ctx.mogi.collect_points(tablestring)
         except ValueError as e:
             return await ctx.respond("Invalid tablestring format.")
 
         # obtain the placements from the collected points
-        placements = list(get_placements_from_scores(mogi.collected_points).values())
+        placements = list(
+            get_placements_from_scores(ctx.mogi.collected_points).values()
+        )
 
         # break down names and mmrs of all players
-        all_player_mmrs = [player.mmr for player in mogi.players]
+        all_player_mmrs = [player.mmr for player in ctx.mogi.players]
 
         # Calculate MMR results
         results = calculate_mmr(
             all_player_mmrs,
             placements,
-            mogi.format,
+            ctx.mogi.format,
         )
 
         # apply custom mmr scaling
@@ -89,41 +90,41 @@ class calculations(commands.Cog):
 
         # store the results in the mogi, extended for every player
         for delta in results:
-            mogi.mmr_results_by_group.extend([delta] * mogi.format)
+            ctx.mogi.mmr_results_by_group.extend([delta] * ctx.mogi.format)
 
         # Store the Placements in order of Players/Teams
         for place in placements:
-            mogi.placements_by_group.extend([place] * mogi.format)
+            ctx.mogi.placements_by_group.extend([place] * ctx.mogi.format)
 
-        if not len(mogi.mmr_results_by_group) == len(mogi.players):
+        if not len(ctx.mogi.mmr_results_by_group) == len(ctx.mogi.players):
             return await ctx.respond(
                 "Something has gone seriously wrong, the amount of players and the MMR results don't add up. Use /debug to find the issue and contact a moderator."
             )
 
         # DEBUG: remove this in prod
         print(f"All MMRs: {all_player_mmrs}")
-        print(f"Summed Points: {mogi.collected_points}")
-        print(f"Placements Points: {get_placements_from_scores(mogi.collected_points)}")
-        print(f"MMR Changes: {mogi.mmr_results_by_group}")
+        print(f"Summed Points: {ctx.mogi.collected_points}")
+        print(
+            f"Placements Points: {get_placements_from_scores(ctx.mogi.collected_points)}"
+        )
+        print(f"MMR Changes: {ctx.mogi.mmr_results_by_group}")
 
-        file = File(create_table(mogi), filename="table.png")
+        file = File(create_table(ctx.mogi), filename="table.png")
         await ctx.respond(content="# Results", file=file)
 
     @points.command(name="reset", description="Reset collected points")
     @is_mogi_open()
     @is_mogi_in_progress()
     @is_mogi_manager()
-    async def reset(self, ctx: ApplicationContext):
+    async def reset(self, ctx: MogiApplicationContext):
         await ctx.response.defer()
 
-        mogi: Mogi = mogi_manager.get_mogi(ctx.channel.id)
-
-        if not mogi.collected_points:
+        if not ctx.mogi.collected_points:
             return await ctx.respond("No points have been collected yet.")
 
-        mogi.collected_points.clear()
-        mogi.placements_by_group.clear()
-        mogi.mmr_results_by_group.clear()
+        ctx.mogi.collected_points.clear()
+        ctx.mogi.placements_by_group.clear()
+        ctx.mogi.mmr_results_by_group.clear()
 
         await ctx.respond("Points have been reset.")
 
@@ -133,22 +134,21 @@ class calculations(commands.Cog):
     @is_mogi_open()
     @is_mogi_in_progress()
     @is_mogi_manager()
-    async def apply(self, ctx: ApplicationContext):
+    async def apply(self, ctx: MogiApplicationContext):
         await ctx.response.defer()
-        mogi: Mogi = mogi_manager.get_mogi(ctx.channel.id)
 
-        if not mogi.mmr_results_by_group:
+        if not ctx.mogi.mmr_results_by_group:
             return await ctx.respond("No results to apply or already applied")
 
-        if not len(mogi.mmr_results_by_group) == len(mogi.players):
+        if not len(ctx.mogi.mmr_results_by_group) == len(ctx.mogi.players):
             return await ctx.respond(
                 "Something has gone seriously wrong, players and results don't add up. Use /debug to find the issue and contact a moderator."
             )
 
-        await apply_mmr(mogi)
+        await apply_mmr(ctx.mogi)
         await ctx.send("Applied MMR changes ✅")
-        await update_roles(ctx, mogi)
-        mogi.finish()
+        await update_roles(ctx, ctx.mogi)
+        ctx.mogi.finish()
 
         if await confirmation(
             ctx,
