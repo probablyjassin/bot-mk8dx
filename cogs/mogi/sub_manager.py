4f9fcf3761616e755c055e3988b3edcbd202f055
probablyjassin <aouanijassin@gmail.com>
2024-10-31 14:28:24 +0100

    simplify sub command

--- cogs/mogi/sub_manager.py
+++ cogs/mogi/sub_manager.py
@@ -4,15 +4,15 @@ from discord.ext import commands
 from models.PlayerModel import PlayerProfile
 from models.MogiModel import Mogi
 
-from utils.command_helpers.valid_discord_mention import is_discord_mention
+from utils.command_helpers.find_player import search_player
 from utils.data.mogi_manager import mogi_manager
 from utils.data.database import db_players
 from utils.command_helpers.checks import is_mogi_open, is_mogi_in_progress
 
 
-def replace(space, player, sub):
+def recurse_replace(space, player, sub):
     if isinstance(space, list):
-        return [replace(item, player, sub) for item in space]
+        return [recurse_replace(item, player, sub) for item in space]
     else:
         return sub if space == player else space
 
@@ -32,63 +32,33 @@ class sub_manager(commands.Cog):
         self,
         ctx: ApplicationContext,
         player_name: str = Option(
-            str, name="player", description="Player that needs a sub."
+            str, name="player", description="username | @ mention | discord_id"
         ),
         replacement_name: str = Option(
-            str, name="sub", description="Replacement player coming in."
+            str, name="sub", description="username | @ mention | discord_id"
         ),
     ):
         mogi: Mogi = mogi_manager.get_mogi(ctx.channel.id)
 
-        # user can input both discord mention or player name
-        # check for both cases
-        if (
-            not is_discord_mention(player_name)
-            and player_name not in [player.name for player in mogi.players]
-        ) or (
-            is_discord_mention(player_name)
-            and int(player_name.strip("<@>"))
-            not in [player.discord_id for player in mogi.players]
-        ):
+        player_profile = search_player(player_name)
+        replacement_profile = search_player(replacement_name)
+
+        if not player_profile:
+            return await ctx.respond("Player profile not found", ephemeral=True)
+
+        if not replacement_profile:
+            return await ctx.respond("Sub profile not found", ephemeral=True)
+
+        if player_profile not in mogi.players:
             return await ctx.respond("Player not in the mogi", ephemeral=True)
 
-        if (
-            is_discord_mention(player_name)
-            and player_name in [player.name for player in mogi.players]
-        ) or (
-            not is_discord_mention(player_name) and int(player_name.strip("<@>"))
-            if is_discord_mention(player_name)
-            else None in [player.discord_id for player in mogi.players]
-        ):
+        if replacement_profile in mogi.players:
             return await ctx.respond("Sub is already in the mogi.", ephemeral=True)
 
-        player_profile = [
-            player for player in mogi.players if player.name == player_name
-        ][0]
-
-        replacement_profile = PlayerProfile(
-            **db_players.find_one(
-                {
-                    "$or": [
-                        {"name": replacement_name},
-                        {
-                            "discord_id": (
-                                int(replacement_name.strip("<@>"))
-                                if is_discord_mention(replacement_name)
-                                else None
-                            )
-                        },
-                    ]
-                }
-            )
+        mogi.players = recurse_replace(
+            mogi.players, player_profile, replacement_profile
         )
-        if not replacement_profile:
-            return await ctx.respond(
-                "The sub does not have a Lounge Profile", ephemeral=True
-            )
-
-        mogi.players = replace(mogi.players, player_profile, replacement_profile)
-        mogi.teams = replace(mogi.teams, player_profile, replacement_profile)
+        mogi.teams = recurse_replace(mogi.teams, player_profile, replacement_profile)
 
         mogi.subs.append(replacement_profile)
 
@@ -96,8 +66,17 @@ class sub_manager(commands.Cog):
             f"<@{player_profile.discord_id}> has been subbed out for <@{replacement_profile.discord_id}>"
         )
 
-    @replacement.command(name="unsub")
-    async def sub(self, ctx: ApplicationContext):
+    @replacement.command(
+        name="remove_sub",
+        description="Remove a player from the sub list. Will let them lose MMR.",
+    )
+    async def sub(
+        self,
+        ctx: ApplicationContext,
+        player_name: str = Option(
+            str, name="player", description="Player that needs a sub."
+        ),
+    ):
         pass
 
 
--- utils/command_helpers/valid_discord_mention.py
+++
@@ -1,5 +0,0 @@
-import re
-
-
-def is_discord_mention(string):
-    return bool(re.compile(r"^<@!?(\d+)>$").match(string))
