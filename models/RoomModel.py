6d3a314f916ea75069b759f38e10a097b81dc73f
probablyjassin <aouanijassin@gmail.com>
2024-11-03 19:03:31 +0100

    add class model for server room

---
+++ models/RoomModel.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Room:
+    address: str
+    description: str
+    externalGuid: str
+    hasPassword: bool
+    id: str
+    maxPlayers: int
+    name: str
+    netVersion: int
+    owner: str
+    players: list[dict]
+    port: int
+    preferredGameId: int
+    preferredGameName: str
+
+    def most_popular_game(self) -> tuple[int, str]:
+        game_count = {}
+        for player in self.players:
+            game_name = player["gameName"]
+            if game_name in game_count:
+                game_count[game_name] += 1
+            else:
+                game_count[game_name] = 1
+        most_popular = max(game_count, key=game_count.get)
+        return game_count[most_popular], most_popular
