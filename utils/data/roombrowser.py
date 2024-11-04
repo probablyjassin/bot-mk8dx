06218db9c814dae6f2291a1fc9158d6e2064bf93
Jässin Aouani <aouanijassin@gmail.com>
2024-11-04 14:01:36 +0100

    use enum for get_room_info

--- utils/data/roombrowser.py
+++ utils/data/roombrowser.py
@@ -1,11 +1,17 @@
 import requests
-from typing import Literal
+from enum import Enum
 
 from models.RoomModel import Room
 from config import YUZU_API_URL, YUZU_SERVER_IP, SERVER_MAIN_PORT, SERVER_LOUNGE_PORT
 
 
-def get_room_info(server: str = Literal["main", "lounge"]) -> Room:
+class ServerType(Enum):
+    MAIN = "main"
+    LOUNGE = "lounge"
+
+
+def get_room_info(server: ServerType) -> Room:
+
     try:
         data: dict = (requests.get(f"http://{YUZU_API_URL}/lobby")).json()
         servers: list[dict] = data.get("rooms", [])
@@ -17,7 +23,7 @@ def get_room_info(server: str = Literal["main", "lounge"]) -> Room:
                 if room["address"] == YUZU_SERVER_IP
                 and (
                     room["port"] == SERVER_MAIN_PORT
-                    if server == "main"
+                    if server == ServerType.MAIN
                     else room["port"] == SERVER_LOUNGE_PORT
                 )
             ][0]
