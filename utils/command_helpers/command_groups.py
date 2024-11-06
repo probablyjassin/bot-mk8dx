db1d80b6aae934c6eec27bdf9b121548e1bbde8c
Jässin Aouani <aouanijassin@gmail.com>
2024-11-06 08:25:30 +0100

    move another commandgroup

--- utils/command_helpers/command_groups.py
+++ utils/command_helpers/command_groups.py
@@ -8,6 +8,10 @@ manage = SlashCommandGroup(
     "manage", "Commands for mogi managers to manage players and such."
 )
 
+team = SlashCommandGroup(
+    name="team", description="Edit Team tags and apply/remove roles"
+)
+
 replacement = manage.create_subgroup(
     "replacement", "Substitute a player who can't play anymore."
 )
