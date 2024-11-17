a6d77419e5da774d02ba715914c110c7b8b1b327
probablyjassin <aouanijassin@gmail.com>
2024-11-17 18:29:22 +0100

    use asyncio.lock for voting

--- utils/command_helpers/vote_btn_callback.py
+++ utils/command_helpers/vote_btn_callback.py
@@ -1,9 +1,12 @@
 import math
 import random
+import asyncio
 
 from discord import Interaction
 
 from models.MogiModel import Mogi
+
+voters_lock = asyncio.Lock()
 
 
 async def button_callback(
@@ -22,45 +25,48 @@ async def button_callback(
         return await interaction.respond("Can't vote on that", ephemeral=True)
 
     # cast vote
-    mogi.votes[label.lower()] += 1
-    mogi.voters.append(interaction.user.id)
+    async with voters_lock:
+        mogi.votes[label.lower()] += 1
+        mogi.voters.append(interaction.user.id)
 
-    # respond
-    await interaction.respond(f"Voted for {label}", ephemeral=True)
+        # respond
+        await interaction.respond(f"Voted for {label}", ephemeral=True)
 
-    # check if vote is decided
-    if not (len(mogi.voters) >= len(mogi.players)) or not (
-        mogi.votes[max(mogi.votes, key=mogi.votes.get)]
-        >= math.floor(len(mogi.players) / 2) + 1
-    ):
-        return
+        # check if vote is decided
+        if not (len(mogi.voters) >= len(mogi.players)) or not (
+            mogi.votes[max(mogi.votes, key=mogi.votes.get)]
+            >= math.floor(len(mogi.players) / 2) + 1
+        ):
+            return
 
-    # get winning format
-    max_score = max(mogi.votes.values())
-    winners = [player for player, score in mogi.votes.items() if score == max_score]
+        # get winning format
+        max_score = max(mogi.votes.values())
+        winners = [player for player, score in mogi.votes.items() if score == max_score]
 
-    MOGI_FORMAT = None
-    FORMAT_STR = None
+        MOGI_FORMAT = None
+        FORMAT_STR = None
 
-    if len(winners) > 1:
-        await interaction.channel.send(
-            f"# Vote is tied between {' and '.join(winners)}, choosing randomly..."
+        if len(winners) > 1:
+            await interaction.channel.send(
+                f"# Vote is tied between {' and '.join(winners)}, choosing randomly..."
+            )
+
+            chosen_winner = random.choice(winners)
+            MOGI_FORMAT = int(chosen_winner[0]) if chosen_winner[0].isnumeric() else 1
+            FORMAT_STR = chosen_winner
+        else:
+            MOGI_FORMAT = int(winners[0][0]) if winners[0][0].isnumeric() else 1
+            FORMAT_STR = winners[0]
+
+        # start playing mogi
+        mogi.play(MOGI_FORMAT)
+
+        lineup = ""
+        for i, team in enumerate(mogi.teams):
+            lineup += (
+                f"{i}. {', '.join([f'<@{player.discord_id}>' for player in team])}\n"
+            )
+
+        return await interaction.message.channel.send(
+            f"# Mogi starting!\n## Format: {FORMAT_STR}\n### Lineup:\n{lineup}"
         )
-
-        chosen_winner = random.choice(winners)
-        MOGI_FORMAT = int(chosen_winner[0]) if chosen_winner[0].isnumeric() else 1
-        FORMAT_STR = chosen_winner
-    else:
-        MOGI_FORMAT = int(winners[0][0]) if winners[0][0].isnumeric() else 1
-        FORMAT_STR = winners[0]
-
-    # start playing mogi
-    mogi.play(MOGI_FORMAT)
-
-    lineup = ""
-    for i, team in enumerate(mogi.teams):
-        lineup += f"{i}. {', '.join([f'<@{player.discord_id}>' for player in team])}\n"
-
-    return await interaction.message.channel.send(
-        f"# Mogi starting!\n## Format: {FORMAT_STR}\n### Lineup:\n{lineup}"
-    )
