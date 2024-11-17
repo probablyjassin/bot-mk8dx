455e1b30af93e3480fd8c967adbac2e0e6a23a5f
probablyjassin <aouanijassin@gmail.com>
2024-11-17 20:44:23 +0100

    bruv

--- cogs/profiles/register.py
+++ cogs/profiles/register.py
@@ -78,6 +78,7 @@ class register(commands.Cog):
             )
         await member.add_roles(ctx.get_lounge_role("Lounge Player"))
         await member.add_roles(ctx.get_lounge_role("Lounge - Silver"))
+        await ctx.send("Cooking...")
         await ctx.respond(
             f"{member.mention} is now registered for Lounge as {username}\n You can view your profile at https://mk8dx-yuzu.github.io/{username}",
             ephemeral=True,
