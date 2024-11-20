af23f2e24e107afd4f8a0fab4d2e02a4e25e9dc2
probablyjassin <aouanijassin@gmail.com>
2024-11-20 19:58:51 +0100

    don't allow edit mmr while player in mogi

--- cogs/profiles/penalties.py
+++ cogs/profiles/penalties.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from models.CustomMogiContext import MogiApplicationContext
 from models.PlayerModel import PlayerProfile
 
+from utils.data.mogi_manager import mogi_manager
 from utils.command_helpers.find_player import search_player
 from utils.command_helpers.checks import is_moderator
 
@@ -48,6 +49,12 @@ class penalties(commands.Cog):
     ):
         player: PlayerProfile = search_player(searched_player)
         mrboost: PlayerProfile = search_player("mrboost")
+
+        for mogi in list(mogi_manager.mogi_registry.values()):
+            if player in mogi.players:
+                return await ctx.respond(
+                    f"Can't change MMR of <@{player.discord_id}> while they are in a mogi"
+                )
 
         player.mmr = player.mmr - abs(mmr)
         mrboost.mmr = mrboost.mmr + abs(mmr)
