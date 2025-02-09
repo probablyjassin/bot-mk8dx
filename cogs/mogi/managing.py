901ce8ef47f6691d64d1a58df44d07b3d5173b02
NotNiall <niall@nya.care>
2025-02-09 17:10:25 +0000

    OMGOMGMGOMGOMGOM NIALL FIXED IT :fire:

--- cogs/mogi/managing.py
+++ cogs/mogi/managing.py
@@ -42,8 +42,12 @@ class managing(commands.Cog):
             return await ctx.respond("Player is already in the mogi", ephemeral=True)
 
         ctx.mogi.players.append(player_profile)
+        await (await ctx.guild.fetch_member(player.discord_id)).add_roles(
+            ctx.inmogi_role, ctx.reopening_role
+        )
+
         await ctx.respond(
-            f"<@{player_profile.discord_id}> joined the mogi! (against their will)"
+            f"<@{player_profile.discord_id}> joined the mogi! (against their will)",
         )
 
     @manage.command(name="remove", description="Remove a player from the current mogi")
