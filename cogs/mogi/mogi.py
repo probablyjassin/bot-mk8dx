b8c15979f8c225d0556d5dbf95a6017b8a5e454f
probablyjassin <aouanijassin@gmail.com>
2025-05-05 22:49:15 +0200

    add testing for move command

--- cogs/mogi/mogi.py
+++ cogs/mogi/mogi.py
@@ -1,11 +1,11 @@
-from discord import slash_command
+from discord import slash_command, Option, TextChannel
 from discord.ext import commands
 from discord.utils import get
 
 from models.CustomMogiContext import MogiApplicationContext
 from utils.data.mogi_manager import mogi_manager
 from utils.command_helpers.confirm import confirmation
-from utils.command_helpers.checks import is_mogi_not_in_progress
+from utils.command_helpers.checks import is_mogi_not_in_progress, is_mogi_open, is_admin
 from utils.command_helpers.team_roles import remove_team_roles
 from utils.command_helpers.find_player import get_guild_member
 
@@ -45,6 +45,32 @@ class mogi(commands.Cog):
             return await ctx.respond("# This channel's Mogi has been closed.")
 
         await ctx.respond("Canceled")
+
+    @slash_command(
+        name="move", description="Move the current channel's mogi to another channel"
+    )
+    @is_mogi_open()
+    @is_admin()
+    async def move(
+        self,
+        ctx: MogiApplicationContext,
+        to_channel: TextChannel = Option(
+            TextChannel,
+            name="to_channel",
+            description="The channel to move the mogi to",
+        ),
+    ):
+        await ctx.interaction.response.defer()
+
+        if mogi_manager.get_mogi(to_channel.id):
+            return await ctx.respond("A mogi in the target channel is already open")
+
+        await ctx.send(ctx.mogi.channel_id)
+        await ctx.send("current mogi id => new channel's id")
+        await ctx.respond(
+            "new current channel mogi id:",
+            ctx.mogi.channel_id if ctx.mogi else "no mogi here",
+        )
 
 
 def setup(bot: commands.Bot):
