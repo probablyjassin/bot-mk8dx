fa626894b743edd296a90ebb314bf53eee92da82
Jässin Aouani <aouanijassin@gmail.com>
2025-05-07 15:11:51 +0200

    fix collection name typo for archived

--- cogs/profiles/archive.py
+++ cogs/profiles/archive.py
@@ -45,7 +45,7 @@ class archive(commands.Cog):
             str, name="player", description="username | @ mention | discord_id"
         ),
     ):
-        player: PlayerProfile = search_player(searched_player, archive_only=True)
+        player: PlayerProfile = search_player(searched_player, archived_only=True)
 
         if not player:
             return await ctx.respond("Couldn't find that player")
