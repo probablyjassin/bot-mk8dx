e3894fc27f6d261ef8ab0692a7d640b846684fbe
probablyjassin <aouanijassin@gmail.com>
2025-05-10 15:26:46 +0200

    move classes archive_type and sort_type to toplevel

--- utils/data/data_manager.py
+++ utils/data/data_manager.py
@@ -4,9 +4,22 @@ from enum import Enum
 
 from dataclasses import dataclass
 from models.PlayerModel import PlayerProfile
-from models.MogiModel import Mogi
+from models.MogiModel import MogiHistoryData
 
 from utils.data._database import db_players, db_archived, db_mogis
+
+
+class archive_type(Enum):
+    NONE = "none"
+    INCLUDE = "include"
+    ONLY = "only"
+
+
+class sort_type(Enum):
+    MMR = "MMR"
+    WINS = "Wins"
+    LOSSES = "Losses"
+    WINRATE = "Winrate %"
 
 
 @dataclass
@@ -14,26 +27,42 @@ class DataManager:
     def __init__(self):
         pass
 
-    class archive_type(Enum):
-        NONE = "none"
-        INCLUDE = "include"
-        ONLY = "only"
-
-    class sort_type(Enum):
-        MMR = "MMR"
-        WINS = "Wins"
-        LOSSES = "Losses"
-        WINRATE = "Winrate %"
-
     def find_player(
         query: int | Int64 | str, archive: archive_type = archive_type.NONE
     ) -> PlayerProfile | None:
-        pass
+        query_criteria = {
+            "$or": [
+                {"name": (query.lower() if isinstance(query, str) else query)},
+                {
+                    "discord_id": (
+                        Int64(query)
+                        if isinstance(query, Int64 | int)
+                        else (
+                            Int64(query.strip("<@!>"))
+                            if query.strip("<@!>").isdigit()
+                            else None
+                        )
+                    )
+                },
+            ]
+        }
+
+        pipeline = []
+        if archive == archive_type.NONE:
+            pipeline.append({"$unionWith": {"coll": "archived"}})
+        pipeline.extend([{"$match": query_criteria}, {"$limit": 1}])
+
+        potential_player = next(db_players.aggregate(pipeline), None)
+
+        return PlayerProfile(**potential_player) if potential_player else None
 
     def get_all_players(
-        archive: archive_type = archive_type.NONE,
+        archive: archive_type = archive_type.NONE, with_id: bool = False
     ) -> list[PlayerProfile] | None:
-        pass
+        return [
+            PlayerProfile.from_json(player)
+            for player in list(db_players.find({}, {"_id": 0} if with_id else {}))
+        ]
 
     def get_leaderboard(
         page_index: int, sort: sort_type = sort_type.MMR
@@ -57,6 +86,7 @@ class DataManager:
         pipeline.append(
             {
                 "$project": {
+                    "_id": 0,
                     "name": 1,
                     "mmr": 1,
                     "history": 1,
@@ -117,6 +147,33 @@ class DataManager:
 
     def delete_player(query: int | Int64 | str):
         pass
+
+    def get_all_mogis(with_id: bool = False) -> list[MogiHistoryData]:
+        return [
+            MogiHistoryData.from_dict(mogi)
+            for mogi in list(db_mogis.find({}, {"_id": 0} if with_id else {}))
+        ]
+
+    def add_mogi_history(
+        started_at: int,
+        finished_at: int,
+        player_ids: list[int],
+        format: int,
+        subs: int,
+        results: list[int],
+        disconnections: int,
+    ) -> None:
+        db_mogis.insert_one(
+            {
+                "started_at": started_at,
+                "finished_at": finished_at,
+                "player_ids": player_ids,
+                "format": format,
+                "subs": subs,
+                "results": results,
+                "disconnections": disconnections,
+            }
+        )
 
 
 data_manager = DataManager({})
