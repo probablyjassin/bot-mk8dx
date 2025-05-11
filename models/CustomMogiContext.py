de0fbb8b34b628c7d7e0788b610297680288a653
probablyjassin <aouanijassin@gmail.com>
2025-05-11 15:55:23 +0200

    add player_discord field to custom mogi context

--- models/CustomMogiContext.py
+++ models/CustomMogiContext.py
@@ -1,4 +1,4 @@
-import discord
+from discord import ApplicationContext, Member, Guild, Role, TextChannel
 from discord.utils import get
 
 from utils.data.mogi_manager import mogi_manager
@@ -8,9 +8,11 @@ from models.PlayerModel import PlayerProfile
 from config import GUILD_IDS, RESULTS_CHANNEL_ID, REGISTER_CHANNEL_ID
 
 
-class MogiApplicationContext(discord.ApplicationContext):
+class MogiApplicationContext(ApplicationContext):
     """## `discord.ApplicationContext` with custom Lounge attributes:
     - `mogi`: `Mogi` object of the channel
+    - `player`: `PlayerProfile` object passed down by the `@with_player` decorator, `None` if not used
+    - `player_discord`: `Member` object passed down by the `@with_player` decorator, `None` if not used
     - `main_guild`: `discord.Guild` object of the main guild
     - `inmogi_role`: `discord.Role` object of the InMogi role
     - `get_lounge_role(name: str)`: method to get a role by name
@@ -37,16 +39,17 @@ class MogiApplicationContext(discord.ApplicationContext):
 
         self.mogi: Mogi | None = mogi_manager.get_mogi(self.channel.id)
         self.player: PlayerProfile | None = None
+        self.player_discord: Member | None = None
 
-        self.main_guild: discord.Guild = get(self.bot.guilds, id=GUILD_IDS[0])
-        self.inmogi_role: discord.Role = get(self.main_guild.roles, name="InMogi")
+        self.main_guild: Guild = get(self.bot.guilds, id=GUILD_IDS[0])
+        self.inmogi_role: Role = get(self.main_guild.roles, name="InMogi")
 
-        self.register_channel: discord.TextChannel = get(
+        self.register_channel: TextChannel = get(
             self.main_guild.text_channels, id=REGISTER_CHANNEL_ID
         )
-        self.results_channel: discord.TextChannel = get(
+        self.results_channel: TextChannel = get(
             self.main_guild.text_channels, id=RESULTS_CHANNEL_ID
         )
 
-    def get_lounge_role(self, name: str) -> discord.Role:
+    def get_lounge_role(self, name: str) -> Role:
         return get(self.main_guild.roles, name=name)
