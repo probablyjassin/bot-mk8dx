938cd3e90fbd974f298f4d46e6ec55da0e60648f
probablyjassin <aouanijassin@gmail.com>
2025-05-18 12:02:34 +0200

    fix /status for no mogi open

--- cogs/rooms.py
+++ cogs/rooms.py
@@ -47,9 +47,7 @@ class rooms(commands.Cog):
 
     @slash_command(name="status", description="Get the status of the current mogi")
     async def status(self, ctx: MogiApplicationContext):
-        room: Room = (
-            ctx.mogi.room if ctx.mogi.room else get_room_info(ServerType.LOUNGE)
-        )
+        room: Room = ctx.mogi.room if ctx.mogi and ctx.mogi.room else None
         room.refresh()
 
         data = {}
