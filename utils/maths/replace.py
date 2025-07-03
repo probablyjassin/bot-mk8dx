bfa0df9e4dfb9f8600d928fcf62c391cb9b5c288
probablyjassin <aouanijassin@gmail.com>
2025-07-03 12:54:46 +0200

    might have fixed occasional issues with /sub

--- utils/maths/replace.py
+++ utils/maths/replace.py
@@ -2,9 +2,9 @@ from models.PlayerModel import PlayerProfile
 
 
 def recurse_replace(
-    space: list | list[list], player: PlayerProfile, sub: PlayerProfile
+    space: list | list[list] | PlayerProfile, player: PlayerProfile, sub: PlayerProfile
 ):
     if isinstance(space, list):
         return [recurse_replace(item, player, sub) for item in space]
-    else:
-        return sub if space == player else space
+    elif hasattr(space, "discord_id") and hasattr(player, "discord_id"):
+        return sub if space.discord_id == player.discord_id else space
