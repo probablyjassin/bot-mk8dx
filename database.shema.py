7ecc2407a913cee4d59363f38a707d1c7d1349f5
probablyjassin <aouanijassin@gmail.com>
2025-07-22 22:55:44 +0200

    add database shema file for clarity

---
+++ database.shema.py
@@ -0,0 +1,26 @@
+# This file is unused. It just presents the schema of the MongoDB-Collections. Object IDs are of course omitted.
+
+{
+    # Players
+    "players": [
+        {
+            "name": "joe",
+            "discord_id": 1234567890,
+            "mmr": 2034,
+            "history": [34],
+            "disconnects": 5,
+            "joined": 0000000000,
+            "formats": {1: 0, 2: 0, 3: 1, 4: 0, 5: 0, 6: 0},
+        }
+    ],
+    # Mogis
+    "mogis": {
+        "started_at": 000000000,
+        "finished_at": 999999999,
+        "player_ids": [123980127931, 781923691628312, 3312367211293],
+        "format": 1,
+        "subs": 9,
+        "results": [23, -23, -2],
+        "disconnections": 98,
+    },
+}
