931fef925e9a810a9f1ab8332faf249a7d08df6b
probablyjassin <aouanijassin@gmail.com>
2025-07-23 23:04:49 +0200

    start working on commands to submit screenshots to the bot

---
+++ utils/data/table_manager.py
@@ -0,0 +1,24 @@
+import os
+from discord import Attachment
+from dataclasses import dataclass
+from models.MogiModel import Mogi
+
+
+@dataclass
+class TableManager:
+    def __init__(self):
+        self.ss_dir = "state/screenshots"
+        os.makedirs(self.ss_dir, exist_ok=True)
+
+    async def save_screenshot(
+        self, mogi: Mogi, image: Attachment, races: int, total: int
+    ) -> None:
+        os.makedirs(f"{self.ss_dir}/{mogi.channel_id}", exist_ok=True)
+        filename = f"ss.{races}_races.{total}.png"
+        filepath = f"{self.ss_dir}/{mogi.channel_id}/{filename}"
+
+        with open(filepath, "wb") as f:
+            f.write(await image.read())
+
+
+table_manager = TableManager({})
