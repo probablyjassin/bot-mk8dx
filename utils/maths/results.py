89c6160a7a47ce3db21de40c60648a6c5e9b5d11
probablyjassin <aouanijassin@gmail.com>
2025-07-27 17:33:00 +0200

    add format to results table

--- utils/maths/results.py
+++ utils/maths/results.py
@@ -7,6 +7,8 @@ from utils.maths.mmr_algorithm import calculate_mmr
 from utils.maths.placements import get_placements_from_scores
 from utils.maths.table import create_table
 
+from config import FORMATS
+
 
 async def process_tablestring(ctx: MogiApplicationContext, tablestring: str):
     if not tablestring:
@@ -58,12 +60,16 @@ async def process_tablestring(ctx: MogiApplicationContext, tablestring: str):
         )
         return False
 
+    key_to_format = {
+        int(format[0]) if format[0].isdigit() else 1: format for format in FORMATS
+    }
+
     # Store the date of the results
     file = File(await create_table(ctx.mogi), filename="table.png")
     message = await ctx.results_channel.send(
         content=f"# Results - {time.strftime('%d.%m.%y')}\n"
         f"Duration: {int((ctx.mogi.finished_at - ctx.mogi.started_at) / 60)} minutes"
-        f"{' | (MINI MOGI)' if ctx.mogi.is_mini else ''}",
+        f"{' | (MINI MOGI)' if ctx.mogi.is_mini else '(' + key_to_format[ctx.mogi.format] + ')'}",
         file=file,
     )
 
