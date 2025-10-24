f5ae5bd012d8c3b884e876b9268eff3a02f8cbcb
probablyjassin <aouanijassin@gmail.com>
2025-10-25 00:18:29 +0200

    surely this makes the table reader work without playwright

--- cogs/screenshots/table_read.py
+++ cogs/screenshots/table_read.py
@@ -1,28 +1,18 @@
-from config import ROOMS_CONFIG, LOG_CHANNEL_ID
-
-from discord import SlashCommandGroup, Option, File, Attachment
+from discord import SlashCommandGroup, Option, Attachment
 from discord.ext import commands
 
 from models.CustomMogiContext import MogiApplicationContext
-from models.RoomModel import Room
 
-from utils.data.data_manager import data_manager, archive_type
-from utils.data.mogi_manager import mogi_manager
-from utils.data.state import state_manager
-from utils.command_helpers.confirm import confirmation
 from utils.decorators.checks import (
     is_admin,
 )
-
-
-from utils.command_helpers.update_server_passwords import fetch_server_passwords
 
 
 class table_read(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
 
-    table = SlashCommandGroup(name="table", description="Debugging commands")
+    table = SlashCommandGroup(name="table", description="WIP")
 
     @table.command(
         name="read",
