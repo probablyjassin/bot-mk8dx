a292fd0e60892ad5574f19e97198fbcc7c324bf9
probablyjassin <aouanijassin@gmail.com>
2025-10-26 11:39:16 +0100

    add back guild display cog

---
+++ cogs/guilds/display.py
@@ -0,0 +1,34 @@
+from discord import slash_command, Option, Attachment
+from discord.ext import commands
+
+from models.CustomMogiContext import MogiApplicationContext
+from models.GuildModel import Guild
+
+from utils.decorators.checks import (
+    is_admin,
+)
+
+
+class display(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @slash_command(
+        name="guild",
+        description="View your guild or a guild of your choice",
+    )
+    @is_admin()
+    async def guild(
+        self,
+        ctx: MogiApplicationContext,
+        guild: str = Option(
+            str,
+            "Name",
+            required=False,
+        ),
+    ):
+        return await ctx.respond("meow")
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(display(bot))
