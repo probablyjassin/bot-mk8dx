f72e4996c56ef60859906256979f0f3aaab8c467
probablyjassin <aouanijassin@gmail.com>
2025-10-26 13:05:32 +0100

    add __init__ for data

---
+++ utils/data/__init__.py
@@ -0,0 +1,16 @@
+# from ._database import client, db_mogis, db_players, db_archived, db_mogis
+from .data_manager import data_manager, archive_type
+from .mogi_manager import mogi_manager
+from .roombrowser import get_room_info, ServerType
+from .state import state_manager
+from .table_manager import table_manager
+
+__all__ = [
+    "data_manager",
+    "archive_type",
+    "mogi_manager",
+    "get_room_info",
+    "ServerType",
+    "state_manager",
+    "table_manager",
+]
