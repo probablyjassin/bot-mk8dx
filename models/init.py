b20dda371c41be6a037284cf8294ab30581894ba
probablyjassin <aouanijassin@gmail.com>
2025-10-26 11:47:15 +0100

    create models/init.py to simplify model imports (fire)

---
+++ models/init.py
@@ -0,0 +1,18 @@
+from CustomMogiContext import ApplicationContext
+from PlayerModel import PlayerProfile
+from MogiModel import Mogi, MogiHistoryData
+from GuildModel import Guild
+from VoteModel import Vote
+from RankModel import Rank
+from RoomModel import Room
+
+__all__ = [
+    "ApplicationContext",
+    "PlayerProfile",
+    "Mogi",
+    "MogiHistoryData",
+    "Guild",
+    "Vote",
+    "Rank",
+    "Room",
+]
