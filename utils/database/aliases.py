09c4cb479eadd0e6b3c95db5667cd8e684fd347c
probablyjassin <aouanijassin@gmail.com>
2025-11-08 11:29:33 +0100

    make aliases.py async

--- utils/database/aliases.py
+++ utils/database/aliases.py
@@ -6,12 +6,12 @@ if TYPE_CHECKING:
     from models.PlayerModel import PlayerProfile
 
 
-def set_player_alias(player: "PlayerProfile", new_alias: str) -> None:
-    db_aliases.update_one(
+async def set_player_alias(player: "PlayerProfile", new_alias: str) -> None:
+    await db_aliases.update_one(
         {"name": player.name}, {"$set": {"alias": new_alias}}, upsert=True
     )
 
 
-def get_all_aliases() -> dict[str, str]:
-    entries = list(db_aliases.find({}))
+async def get_all_aliases() -> dict[str, str]:
+    entries = await db_aliases.find({}).to_list(length=None)
     return {entry["name"]: entry["alias"] for entry in entries}
