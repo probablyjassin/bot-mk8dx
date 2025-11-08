951e5ad0e93c8cc5081f39f4080aef70a99735ee
probablyjassin <aouanijassin@gmail.com>
2025-11-08 23:04:20 +0100

    experiment: add sqrt teamsize multiplyer to guild mmr results

--- utils/maths/guild_mogi_mmrs.py
+++ utils/maths/guild_mogi_mmrs.py
@@ -1,3 +1,4 @@
+import math
 from models.GuildModel import Guild
 from models.PlayerModel import PlayerProfile
 from .mmr_algorithm import calculate_mmr
@@ -8,13 +9,16 @@ async def guild_calc_new_mmr(
     playing_players_per_guild: list[list[PlayerProfile]],
     guild_placements: list[int],
 ):
+    team_size: int = len(playing_players_per_guild[0])
+
     average_active_player_mmrs: list[int] = [
         sum(player.mmr for player in guild_players) / len(guild_players)
         for guild_players in playing_players_per_guild
     ]
 
     guild_mmrs_for_calculation: list[int] = [
-        guild.mmr + average_active_player_mmrs[i] for i, guild in enumerate(guilds)
+        math.sqrt(team_size)
+        * (guild.mmr + average_active_player_mmrs[i] for i, guild in enumerate(guilds))
     ]
 
     return calculate_mmr(guild_mmrs_for_calculation, guild_placements, 1)
