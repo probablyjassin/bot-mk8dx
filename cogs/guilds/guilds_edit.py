67530031285cf5f479a10d9b63a687d2aa08906b
probablyjassin <aouanijassin@gmail.com>
2025-11-12 16:43:43 +0100

    message fix on /guildedit leave

--- cogs/guilds/guilds_edit.py
+++ cogs/guilds/guilds_edit.py
@@ -34,7 +34,9 @@ class guilds_edit(commands.Cog):
             await ctx.user.remove_roles(lounge_guild_role)
 
         await data_manager.Guilds.remove_member(ctx.lounge_guild, ctx.user.id)
-        await ctx.respond(f"{ctx.user.mention} left the guild **{ctx.guild.name}**.")
+        await ctx.respond(
+            f"{ctx.user.mention} left the guild **{ctx.lounge_guild.name}**."
+        )
 
     @guildedit.command(
         name="change-name",
