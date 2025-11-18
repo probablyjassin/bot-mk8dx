713d401ea0b757603b0c98bc171e5a031c8ee09e
probablyjassin <aouanijassin@gmail.com>
2025-11-18 15:58:51 +0100

    implement `@with_player` in `/player`

--- cogs/profiles/player.py
+++ cogs/profiles/player.py
@@ -1,10 +1,11 @@
-from discord import slash_command, Option, ButtonStyle, Embed, Colour, Member
+from discord import slash_command, Option, ButtonStyle, Embed, Colour
 from discord.ui import View, Button
 from discord.ext import commands
 
 from models import MogiApplicationContext, PlayerProfile, Rank
 from database.types import archive_type
 from utils.data import data_manager
+from utils.decorators import with_player
 from utils.command_helpers import player_name_autocomplete
 
 from datetime import datetime
@@ -16,6 +17,7 @@ class player(commands.Cog):
         self.bot: commands.Bot = bot
 
     @slash_command(name="player", description="Show a player and their stats")
+    @with_player(query_varname="searched_name")
     async def player(
         self,
         ctx: MogiApplicationContext,
@@ -27,13 +29,6 @@ class player(commands.Cog):
             autocomplete=player_name_autocomplete,
         ),
     ):
-        player: PlayerProfile = await data_manager.Players.find(
-            searched_name or Int64(ctx.author.id), archive=archive_type.INCLUDE
-        )
-
-        if not player:
-            return await ctx.respond("Couldn't find that player")
-
         class PlayerView(View):
             def __init__(self):
                 super().__init__(timeout=None)
@@ -41,28 +36,28 @@ class player(commands.Cog):
                     Button(
                         label="View on Website",
                         style=ButtonStyle.link,
-                        url=f"https://mk8dx-yuzu.github.io/{player.name}",
+                        url=f"https://mk8dx-yuzu.github.io/{ctx.player.name}",
                     )
                 )
 
         embed = Embed(
-            title=f"{player.name}",
+            title=f"{ctx.player.name}",
             description="",
             color=Colour.blurple(),
         )
-        embed.add_field(name="Discord", value=f"<@{player.discord_id}>")
+        embed.add_field(name="Discord", value=f"<@{ctx.player.discord_id}>")
 
-        if getattr(player, "joined", None):
+        if getattr(ctx.player, "joined", None):
             embed.add_field(
                 name="Joined",
-                value=f"{datetime.fromtimestamp(player.joined).strftime('%b %d %Y')}",
+                value=f"{datetime.fromtimestamp(ctx.player.joined).strftime('%b %d %Y')}",
             )
 
-        player_rank: Rank = Rank.getRankByMMR(player.mmr)
+        player_rank: Rank = Rank.getRankByMMR(ctx.player.mmr)
         embed.add_field(name="Rank", value=player_rank.rankname)
 
-        player_wins = len([delta for delta in player.history if delta >= 0])
-        player_losses = len([delta for delta in player.history if delta < 0])
+        player_wins = len([delta for delta in ctx.player.history if delta >= 0])
+        player_losses = len([delta for delta in ctx.player.history if delta < 0])
         embed.add_field(name="Wins", value=player_wins)
         embed.add_field(name="Losses", value=player_losses)
 
@@ -83,16 +78,16 @@ class player(commands.Cog):
             + "%",
         )
 
-        embed.add_field(name="MMR", value=f"{player.mmr}")
+        embed.add_field(name="MMR", value=f"{ctx.player.mmr}")
 
-        if getattr(player, "history", None):
+        if getattr(ctx.player, "history", None):
             embed.add_field(
                 name="History (last 5)",
-                value=", ".join(map(str, player.history[-5:])),
+                value=", ".join(map(str, ctx.player.history[-5:])),
             )
 
-        if getattr(player, "formats", None):
-            most_p_format: str = max(player.formats, key=player.formats.get)
+        if getattr(ctx.player, "formats", None):
+            most_p_format: str = max(ctx.player.formats, key=ctx.player.formats.get)
             name = "Mini Mogi (FFA)"
             if most_p_format == "1":
                 name = "FFA"
@@ -100,19 +95,21 @@ class player(commands.Cog):
                 name = f"{most_p_format}v{most_p_format}"
             embed.add_field(
                 name="Most played Format",
-                value=name if max(player.formats.values()) > 0 else "None played yet",
+                value=(
+                    name if max(ctx.player.formats.values()) > 0 else "None played yet"
+                ),
             )
 
-        if getattr(player, "inactive", None):
+        if getattr(ctx.player, "inactive", None):
             embed.add_field(name="Archived", value="Account is not active")
 
-        if getattr(player, "suspended", None):
+        if getattr(ctx.player, "suspended", None):
             embed.add_field(name="Suspended", value="Account suspended from Lounge")
 
-        if getattr(player, "disconnects", None):
-            embed.add_field(name="DCd", value=f"{player.disconnects} times")
+        if getattr(ctx.player, "disconnects", None):
+            embed.add_field(name="DCd", value=f"{ctx.player.disconnects} times")
 
-        if player_guild := await player.fetch_guild():
+        if player_guild := await ctx.player.fetch_guild():
             embed.add_field(name="Guild", value=f"**{player_guild.name}**")
 
         embed.set_author(
@@ -124,7 +121,9 @@ class player(commands.Cog):
             url=f"https://raw.githubusercontent.com/mk8dx-yuzu/ranks/refs/heads/main/{player_rank.rankname}.png"
         )
 
-        await ctx.respond(f"# {player.name} - overview", embed=embed, view=PlayerView())
+        await ctx.respond(
+            f"# {ctx.player.name} - overview", embed=embed, view=PlayerView()
+        )
 
 
 def setup(bot: commands.Bot):
