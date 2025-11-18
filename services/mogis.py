499731f4409701cf4735d5f176b684a0ea38a8e9
probablyjassin <aouanijassin@gmail.com>
2025-11-18 16:22:23 +0100

    prepare to replace `data_manager`

--- services/mogis.py
+++ services/mogis.py
@@ -1,5 +1,5 @@
 from typing import TYPE_CHECKING
-from utils.data.data_manager import data_manager
+from database import mogis
 
 if TYPE_CHECKING:
     from models.MogiModel import MogiHistoryData
@@ -8,8 +8,36 @@ if TYPE_CHECKING:
 async def get_all_mogi_history(
     with_id: bool = False, as_json: bool = False
 ) -> list["MogiHistoryData"] | list[dict]:
-    return await data_manager.Mogis.get_all_mogis(with_id=with_id, as_json=as_json)
+    return await mogis.get_all_mogis(with_id=with_id, as_json=as_json)
 
 
 async def add_bulk_mmr(usernames: list[str], amount: int) -> None:
-    return await data_manager.Mogis.bulk_add_mmr(usernames=usernames, amount=amount)
+    return await mogis.bulk_add_mmr(usernames=usernames, amount=amount)
+
+
+async def add_mogi_history(
+    started_at: int,
+    finished_at: int,
+    player_ids: list[int],
+    format: int,
+    subs: int,
+    results: list[int],
+    disconnections: int,
+) -> None:
+    return await mogis.add_mogi_history(
+        started_at=started_at,
+        finished_at=finished_at,
+        player_ids=player_ids,
+        format=format,
+        subs=subs,
+        results=results,
+        disconnections=disconnections,
+    )
+
+
+async def apply_result_mmr(
+    data_to_update_obj: list[dict[str, str | int]], format: int
+) -> None:
+    return await mogis.apply_result_mmr(
+        data_to_update_obj=data_to_update_obj, format=format
+    )
--- services/players.py
+++ services/players.py
@@ -1,7 +1,8 @@
-from utils.data.data_manager import data_manager
+from database import players
 from database.types import archive_type
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
+from bson.int64 import Int64
 
 if TYPE_CHECKING:
     from models.PlayerModel import PlayerProfile
@@ -12,6 +13,45 @@ async def get_all_player_profiles(
     with_id: bool = False,
     as_json: bool = False,
 ) -> list["PlayerProfile"] | list[dict] | None:
-    return await data_manager.Players.get_profiles(
-        archive=archive, with_id=with_id, as_json=as_json
-    )
+    return await players.get_profiles(archive=archive, with_id=with_id, as_json=as_json)
+
+
+async def find_player_profile(
+    query: int | Int64 | str, archive: archive_type = archive_type.NO
+) -> Optional["PlayerProfile"]:
+    return await players.find_player(query=query, archive=archive)
+
+
+async def find_player_profiles_by_ids(
+    player_ids: list[int | Int64],
+) -> list["PlayerProfile"]:
+    return await players.find_list(player_ids=player_ids)
+
+
+async def create_new_player(username: str, discord_id: int) -> None:
+    return await players.create_new_player(username=username, discord_id=discord_id)
+
+
+async def get_all_player_names() -> list[str]:
+    return await players.get_all_player_names()
+
+
+async def count_players() -> int:
+    return await players.count()
+
+
+async def delete_player(player: "PlayerProfile") -> None:
+    return await players.delete_player(player=player)
+
+
+# Internal methods for PlayerModel
+async def set_player_attribute(player: "PlayerProfile", attribute: str, value) -> None:
+    return await players.set_attribute(player=player, attribute=attribute, value=value)
+
+
+async def append_player_history(player: "PlayerProfile", score: int) -> None:
+    return await players.append_history(player=player, score=score)
+
+
+async def count_player_format_played(player: "PlayerProfile", value) -> None:
+    return await players.count_format_played(player=player, value=value)
