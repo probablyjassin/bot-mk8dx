7bf6cd82048a65cb420b899972ecfc72d4df21ee
probablyjassin <aouanijassin@gmail.com>
2025-11-18 15:00:13 +0100

    implement `get_all_mogi_history`

--- services/mogis.py
+++ services/mogis.py
@@ -5,8 +5,10 @@ if TYPE_CHECKING:
     from models.MogiModel import MogiHistoryData
 
 
-async def get_all_mogi_history() -> list["MogiHistoryData"]:
-    return await data_manager.Mogis.get_all_mogis()
+async def get_all_mogi_history(
+    with_id: bool = False, as_json: bool = False
+) -> list["MogiHistoryData"] | list[dict]:
+    return await data_manager.Mogis.get_all_mogis(with_id=with_id, as_json=as_json)
 
 
 async def add_bulk_mmr(usernames: list[str], amount: int) -> None:
