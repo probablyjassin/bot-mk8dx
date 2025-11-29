892408f8379a76c967640df3b148a7018f8a9fb3
probablyjassin <aouanijassin@gmail.com>
2025-11-29 15:34:49 +0100

    WIP embed

--- cogs/mogi/rollback.py
+++ cogs/mogi/rollback.py
@@ -4,7 +4,12 @@ from discord.ext import commands
 from pycord.multicog import subcommand
 
 from utils.decorators import is_admin
-from models import MogiApplicationContext
+from utils.command_helpers import create_embed
+
+from models import MogiApplicationContext, MogiHistoryData
+
+from services.mogis import get_latest_mogi
+from services.players import find_player_profiles_by_ids
 
 
 class rollback(commands.Cog):
@@ -18,7 +23,28 @@ class rollback(commands.Cog):
         description="Admin only: roll back the last mogi's results",
     )
     async def rollback(self, ctx: MogiApplicationContext):
-        return await ctx.respond("meow")
+
+        latest_mogi: MogiHistoryData | None = await get_latest_mogi()
+        if not latest_mogi:
+            return await ctx.respond("Couldn't find a latest mogi in the database.")
+
+        latest_mogi.started_at
+
+        all_players = await find_player_profiles_by_ids(latest_mogi.player_ids)
+
+        last_mogi_embed = create_embed(
+            title="Latest mogi:",
+            description="You are trying to revert the following mogi:",
+            fields={
+                "Started at:" f"<t:{latest_mogi.started_at}>",
+                "Finished at:" f"<t:{latest_mogi.finished_at}>",
+                "Players:"
+                "\n".join(f"<@{player}>" for player in latest_mogi.player_ids),
+                "MMR changes (in player order):" "\n".join(latest_mogi.results),
+            },
+        )
+
+        return await ctx.respond(embed=last_mogi_embed)
 
 
 def setup(bot: commands.Bot):
